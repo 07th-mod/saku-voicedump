e707d1a63b9eae6fa0fbd51a60af5de44108a928
ooa113y <13thSlayer@gmail.com>
2021-01-30 23:38:52 +0300

    Add voice ranges

--- gen_voices.js
+++ gen_voices.js
@@ -15,10 +15,33 @@ for (const v of voiceLines) {
     processedLines.push(v.file)
   }
 }
-fs.writeFileSync('0.utf', script, 'utf-8')
-fs.writeFileSync('needvoices.sh', processedLines.map(x => {
-  const dirname = x.split('/')[0]
-  const origFname = `${x.toLowerCase()}.opus`
-  const destFname = `voice/${x}.ogg`
-  return `mkdir voice/${dirname}; ffmpeg -i ${origFname} ${destFname}`
-}).join('\n'), 'utf-8')
+// The following calculates "voice ranges":
+// it is essentially assumed that the arc will include all the voice lines
+// for each character from their first to their last detected line.
+// Even if some lines were not successfully processed, we'll want to add them to the voice pack *anyway*, so that we can insert them into the script manually later.
+// This is bound to fail on older arcs (as they don't label voices per-character),
+// but fuck it.
+const processedChars = processedLines.map(x => x.split('_'))
+const charValues = {}
+for (const char of processedChars) {
+  const charName = char[0]
+  const charVoice = char[1]
+  if (!charValues[charName]) {
+    charValues[charName] = { min: charVoice, max: charVoice }
+  }
+  if (Number(charVoice) > Number(charValues[charName].max)) {
+    charValues[charName].max = charVoice
+  }
+  if (Number(charVoice) < Number(charValues[charName].min)) {
+    charValues[charName].min = charVoice
+  }
+}
+let out = 'mkdir voice\n'
+for (const char of Object.keys(charValues)) {
+  out += `mkdir voice/${char.split('/')[0]};`
+  out += `for i in {${charValues[char].min}..${charValues[char].max}};`
+  out += `do ffmpeg -i ${char.toLowerCase()}_$i.opus voice/${char}_$i.ogg;done\n`
+}
+fs.writeFileSync('needvoices.sh', out, 'utf-8')
+// Uncomment this to generate the actual script:
+// fs.writeFileSync('0.utf', script, 'utf-8')
